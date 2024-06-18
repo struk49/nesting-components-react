b5a75de75ac39c1d1801678eb70cda0f115dc747
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-06-18 15:57:20 +0000

    nesting components

---
+++ my-app/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import NestingComponents from "./components/NestingComponents";
+
+function App() {
+  return <div className="App">
+      <NestingComponents />
+  </div>;
+}
+
+export default App;
